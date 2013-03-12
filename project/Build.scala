f55c203448ef7c55b89bf8c63377cef45d92c05a
Sean Brady <sean@konciergemd.com>
2013-03-11 18:27:06 -0700

    Example of RequireJS minification working in app

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ApplicationBuild extends Build {
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    requireJs += "main.js"
   ).dependsOn(assets)
 
 }
